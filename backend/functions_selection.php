9db007dd96ccffdcb11f4891e4d0061a0066cacd
master-kirupan <114853337+master-kirupan@users.noreply.github.com>
2022-10-20 12:21:33 +0530

    data added

--- backend/functions_selection.php
+++ backend/functions_selection.php
@@ -15,7 +15,22 @@ if ($Main_form_number == 1 ) {
 						if($activity_number == 5) { header('Location: ../form/1_Student_Achievement/secondary_form_1_2_5.php'); } 
 
 
+} 
 }
-}
+if ($Main_form_number == 2 ) { header('Location: ../form/2_Learning_Teaching_and_Assessment/secondary_form_2.php'); }
+
+if ($Main_form_number == 3 ) { header('Location: .../form/3_Formal_Curriculum_Management/secondary_form_3.php'); }
+
+if ($Main_form_number == 4 ) { header('Location: ../form/4_Co_curricular_Activities/secondary_form_4.php'); }
+
+if ($Main_form_number == 5 ) { header('Location: ../form/5_Student_Welfare/secondary_form_5.php'); }
+
+if ($Main_form_number == 6 ) { header('Location: ../form/6_Leadership_and_Management/secondary_form_6.php'); }
+
+if ($Main_form_number == 7 ) { header('Location: ../form/7_Physical_Resource_Management/secondary_form_7.php'); }
+
+if ($Main_form_number == 8 ) { header('Location: ../form/8_School_and_Community/secondary_form_8.php'); }
+
+
 
 ?>
