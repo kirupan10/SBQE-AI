c764fb85f4980d1b0333285962646ee6e248d044
master-kirupan <114853337+master-kirupan@users.noreply.github.com>
2022-10-21 07:57:10 +0530

    4 curricular data flow fixed

--- form/4_Co_curricular_Activities/function_selection.php
+++ form/4_Co_curricular_Activities/function_selection.php
@@ -1,18 +1,16 @@
 <?php 
 session_start();
- echo $function_number =$_REQUEST['function']; 
+ $Main_form_number =$_REQUEST['function']; // output 2489
 
 
-	if ($function_number > 1 && $function_number < 7  ) {
-	header('Location: main_subjects_form.php');}
+if ($Main_form_number == 1 ) { header('Location: curricular_4.1.php'); }
 
-	if ($function_number > 6 && $function_number < 12  ) {
-	header('Location: other_subjects_form.php');}
+if ($Main_form_number == 2 ) { header('Location: curricular_4.2.php'); }
 
-	if ($function_number > 11 && $function_number < 18  ) {
-	header('Location:  main_subjects_form.php');}
+if ($Main_form_number == 3 ) { header('Location: curricular_4.3.php'); }
 
-	if ($function_number >= 18) {
-		header('Location: optional_subjects_form.php');}
+if ($Main_form_number == 4 ) { header('Location: curricular_4.4.php'); }
+
+if ($Main_form_number == 5 ) { header('Location: curricular_4.5.php'); }
 
 ?>
